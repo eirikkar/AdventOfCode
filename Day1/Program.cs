be0d3486c7fa3de8da8c7de62d84d9c1e41ab0fb
eirikkar <eirikkar@proton.me>
2024-12-06 12:54:20 +0100

    Changed num1, num2 to left, right

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -7,20 +7,19 @@ class Program
     static void Main(string[] args)
     {
         var input = File.ReadLines("numbers.txt").ToList();
-        List<double> num1 = new List<double>();
-        List<double> num2 = new List<double>();
+        List<double> left = new List<double>();
+        List<double> right = new List<double>();
         List<double> sum = new List<double>();
         foreach (var line in input)
         {
             var num = line.Split("   ").Select(double.Parse).ToList();
-            num1.Add(num[0]);
-            num2.Add(num[1]);
+            left.Add(num[0]);
         }
-        num1.Sort();
-        num2.Sort();
         for (int i = 0; i < num1.Count; i++)
+        left.Sort();
+        right.Sort();
         {
-            double diff = Math.Max(num1[i], num2[i]) - Math.Min(num1[i], num2[i]);
+            double diff = Math.Max(left[i], right[i]) - Math.Min(left[i], right[i]);
             sum.Add(diff);
         }
         foreach (var item in sum)
