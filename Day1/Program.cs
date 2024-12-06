b3d610d5094602f82f633ef2823b32a84362be9c
eirikkar <eirikkar@proton.me>
2024-12-06 12:54:49 +0100

    Task2 solution

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -10,22 +10,26 @@ class Program
         List<double> left = new List<double>();
         List<double> right = new List<double>();
         List<double> sum = new List<double>();
+        int similar = 0;
         foreach (var line in input)
         {
             var num = line.Split("   ").Select(double.Parse).ToList();
             left.Add(num[0]);
+            right.Add(num[1]);
         }
-        for (int i = 0; i < num1.Count; i++)
         left.Sort();
         right.Sort();
+        for (int i = 0; i < left.Count; i++)
         {
             double diff = Math.Max(left[i], right[i]) - Math.Min(left[i], right[i]);
             sum.Add(diff);
-        }
-        foreach (var item in sum)
+        } 
+        foreach (int number in left)
         {
-            Console.WriteLine(item);
+            int count = right.Count(x => x == number);
+            similar += number * count;
         }
         Console.WriteLine(sum.Sum());
+        Console.WriteLine(similar);
     }
 }
